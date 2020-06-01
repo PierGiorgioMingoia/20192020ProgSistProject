e17aec5a1b9291fcd96660288a4d1e121efb2291
PierGiorgioMingoia <piergiorgio.mingoia@gmail.com>
2020-06-01 09:51:58 +0200

    Visual studio implementation and Multi conntection server

---
+++ file_watcher/FileWatcher.h
@@ -0,0 +1,16 @@
+//
+// Created by pierg on 31/05/2020.
+//
+
+#ifndef FILE_WATCHER_FILEWATCHER_H
+#define FILE_WATCHER_FILEWATCHER_H
+
+
+
+class FileWatcher {
+
+};
+
+
+
+#endif //FILE_WATCHER_FILEWATCHER_H
