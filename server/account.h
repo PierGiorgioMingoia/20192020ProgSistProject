bb63229eb8a121c46b3ba3a2a1949a182f568a89
PierGiorgioMingoia <piergiorgio.mingoia@gmail.com>
2020-09-26 18:19:26 +0200

    create password function

--- server/account.h
+++ server/account.h
@@ -1,4 +1,4 @@
-#include <map>
+﻿#include <map>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -31,8 +31,23 @@ std::map<std::string, int> readAndStoreAccounts(std::string filePath) {
 }
 
 int computeHashPassword(std::string password) {
-	std::hash <std::string> hashPassword;
-	return hashPassword(password);
+	long long sum = 0;
+	int div = pow(2, 16) - 1;
+	int size = password.size();
+	const char* buf = password.c_str();
+	char c;
+	int vett[] = { 17,349,1153,2081,3361,4517,5573,6733,7411,16127,37199,93911,199933 };
+	int n = password.c_str()[size / 2] % 13;
+	for (int i = 0; i < size; i++)
+	{
+		c = buf[i];
+		sum += sum * vett[n] + c * vett[(n * size) % 13] + (n + size % (i + 1)) * vett[c % 13];
+		n = (n + c + sum) % 13;
+		sum = sum * 999983 + c * 37199; //totalmente a caso, sono 2 numeri primi random che danno una buona distribuzione dei risultati
+	}
+	sum = abs(sum); // voglio solo
+	sum = sum % div;//
+	return int(sum);
 }
 
 bool checkNameAndPassword(std::string name, std::string password, const std::map<std::string, int>& accounts) {
